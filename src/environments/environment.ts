71933c75cde807274a10b2e02ed598dcca92fe5f
raafay ahmed <raafay@arittek.com>
2026-01-04 01:41:51 +0500

    update dependencies and modify environment API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ import pkg from '../../package.json';
 export const environment = {
     production: false,
     VERSION: pkg.version,
-    BASE_URL: 'http://localhost:3000/api/'
+    // BASE_URL: 'http://localhost:3000/api/',
+    BASE_URL: 'https://currency-converter-mu-green.vercel.app/api/'
 }
